f5bd226ab76dc7d941744197e7aec1fa6933910a
sivananda <cvasia549@gmail.com>
2024-10-09 09:00:06 +0530

    first commit

--- app/_shared/components/home/home.tsx
+++ app/_shared/components/home/home.tsx
@@ -1,32 +1,66 @@
 
 /* eslint-disable @next/next/no-img-element */
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./home.module.scss";
 import Link from 'next/link';
-import Image from 'next/image';
+import { FaBars, FaTimes } from "react-icons/fa"
+import { symlink } from 'fs';
 const Home: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   return (
     <div className={styles.home}>
-      <div className={styles.menu}>
-        <div style={{ width: "35%" }}>
-          <img
-            src="/brandstract.png"
-            alt="logo"
-            style={{ width: "22%", marginLeft: "15%" }}
-          />
+      <div className={styles.dblock}>
+        <div className={styles.menu}>
+          <div style={{ width: "35%" }}>
+            <img
+              src="/brandstract.png"
+              alt="logo"
+              style={{ width: "22%", marginLeft: "15%" }}
+            />
+          </div>
+          <div className={styles.dflex}>
+            <Link href="/home" className={styles.linkstle}>Home</Link>
+            <Link href="/home" className={styles.linkstle}>Check ATS Score</Link>
+            <Link href="/home" className={styles.linkstle}>Resume templates</Link>
+            <Link href="/home" className={styles.linkstle}>Pricing</Link>
+          </div>
         </div>
-        <div className={styles.dflex}>
-          <Link href="/home" className={styles.linkstle}>Home</Link>
-          <Link href="/home" className={styles.linkstle}>Check ATS Score</Link>
-          <Link href="/home" className={styles.linkstle}>Resume templates</Link>
-          <Link href="/home" className={styles.linkstle}>Pricing</Link>
+      </div>
+      <div className={styles.menublock}>
+        <div className={styles.flxmenu}>
+          <div style={{ width: "35%" }}>
+            <img
+              src="/brandstract.png"
+              alt="logo"
+              style={{ width: "70%" }}
+            />
+          </div>
+          <div onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? (
+              <FaTimes className="" size={25} />
+            ) : (
+              <FaBars className="" size={25} />
+            )}
+            {menuOpen && (
+              <>
+                <div className={menuOpen ? `${styles.navpostion}` : 'bgnav'}>
+                  <div className={styles.dflex}>
+                    <Link href="/home" className={styles.linkstle}>Home</Link>
+                    <Link href="/home" className={styles.linkstle}>Check ATS Score</Link>
+                    <Link href="/home" className={styles.linkstle}>Resume templates</Link>
+                    <Link href="/home" className={styles.linkstle}>Pricing</Link>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
       <div className={styles.card}>
         <div className={styles.createflx}>
-          <div style={{ width: "50%", display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ width: '41%' }}>
+          <div className={styles.resumeflx}>
+            <div className={styles.ofwidth}>
               <div className={styles.Createfont}>Create a resume you are proud of</div>
               <div className={styles.hassleflx}>
                 <div><img src='/Hourg.png' alt='Hourg' style={{ width: '75%' }} /></div>
@@ -48,9 +82,9 @@ const Home: React.FC = () => {
               </div>
             </div>
           </div>
-          <div style={{ width: "50%", display: 'flex', alignItems: 'center', justifyContent: 'start' }}>
+          <div className={styles.resumeflx}>
             <div className={styles.cardpart2}>
-              <div style={{width:'80%'}}>
+              <div style={{ width: '80%' }}>
                 <div className={styles.Signfont}>Sign in to your account</div>
                 <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem', width: "100%", }}>
                   <div className={styles.cardlink}>
